6abd8263ffd49975088be983e876aee070e3d23e
yukari928 <174513276+yukari928@users.noreply.github.com>
2024-10-20 15:27:09 +0000

    [Update] 4-27

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @books = @user.books
+    @books = @user.books.page(params[:page])
   end
 
   def edit
