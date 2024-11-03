7a2187addd8b29c85add26a6050728884c861cba
yukari928 <174513276+yukari928@users.noreply.github.com>
2024-11-03 15:47:16 +0000

    [Update] 4-27

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
 
   def after_sign_in_path_for(resource)
     flash[:notice] = "Welcome! You have signde up successfully."
-    books_path
+    user_path(current_user)
   end
 
   def after_sign_out_path_for(resource)
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,10 +13,18 @@ class UsersController < ApplicationController
   end
 
   def edit
+    user = User.find(params[:id])
+  unless user.id == current_user.id
+    redirect_to books_path
+  end
     @user = User.find(params[:id])
   end
 
   def update
+    user = User.find(params[:id])
+  unless user.id == current_user.id
+    redirect_to books_path
+  end
     @user = User.find(params[:id])
     @user.update(user_params)
     flash[:notice] = "You have updated user successfully."
